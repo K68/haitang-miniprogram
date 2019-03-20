4e3b7bd64ded2a9d8a21cb9f0aa43996d7a0b5d4
zxyao <1158396760@qq.com>
2019-03-20 16:22:00 +0800

    海棠微信

---
+++ pages/index/index.js
@@ -0,0 +1,33 @@
+//index.js
+//获取应用实例
+let shareValue = {};
+
+Page({
+  onShareAppMessage: function (res) {
+    return {
+      title: shareValue.title,
+      imageUrl: shareValue.imageUrl,
+      path: `pages/index/index?p=${shareValue.path}`,
+      success: function (res) {
+        // 转发成功
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
+  },
+  viewmessageFn: function(e) {
+    if (e.type === 'message' && e.detail && e.detail.data && e.detail.data.length > 0) {
+      shareValue = e.detail.data[e.detail.data.length - 1];
+    }
+  },
+
+  onLoad: function (query) {
+    let path = query.p || '';
+    if (query.scene) {
+      path = decodeURIComponent(query.scene);
+    }
+    const sharePath = path.replace(/-/g, '/'); 
+    this.setData({ Url: `https://hi.amzport.com/app/wechat.html#/${sharePath}?wechat=haitang` });
+  },
+})
